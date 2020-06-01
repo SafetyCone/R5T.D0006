48bedf9a34cd6c0e91e0e6a756e1f87dde4908e0
MinexAutomation <minex.automation@gmail.com>
2020-06-01 12:27:33 -0700

    Added IProgramStartTimeSpecificMessagesOutputDirectoryPathProvider service definition.

---
+++ source/R5T.D0006.Base/Code/Services/Definitions/IProgramStartTimeSpecificMessagesOutputDirectoryPathProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0006
+{
+    public interface IProgramStartTimeSpecificMessagesOutputDirectoryPathProvider
+    {
+        Task<string> GetProgramStartTimeSpecificMessagesOutputDirectoryPath();
+    }
+}
