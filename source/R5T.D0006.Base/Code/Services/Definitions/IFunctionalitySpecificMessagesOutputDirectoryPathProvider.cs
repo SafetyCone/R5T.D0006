bbdf233d63c69cec5475f03c7d45b33b50acb29f
MinexAutomation <minex.automation@gmail.com>
2020-06-01 12:22:05 -0700

    Fixed lack of capitalization defect in code.

--- source/R5T.D0006.Base/Code/Services/Definitions/IFunctionalitySpecificMessagesOutputDirectoryPathProvider.cs
+++ source/R5T.D0006.Base/Code/Services/Definitions/IFunctionalitySpecificMessagesOutputDirectoryPathProvider.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace R5T.D0006
 {
-    public interface IFunctionalitySpecificMessagesOutputDirectorypathProvider
+    public interface IFunctionalitySpecificMessagesOutputDirectoryPathProvider
     {
         Task<string> GetFunctionalitySpecificMessagesOutputDirectoryPath(string functionalityName);
     }
