888a8f6b014f302fd1d3ed52e668f6b2887d8f52
MinexAutomation <minex.automation@gmail.com>
2020-06-01 11:17:20 -0700

    Added IFunctionalitySpecificMessagesOutputDirectorypathProvider servide definition.

---
+++ source/R5T.D0006.Base/Code/Services/Definitions/IFunctionalitySpecificMessagesOutputDirectoryPathProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0006
+{
+    public interface IFunctionalitySpecificMessagesOutputDirectorypathProvider
+    {
+        Task<string> GetFunctionalitySpecificMessagesOutputDirectoryPath(string functionalityName);
+    }
+}
